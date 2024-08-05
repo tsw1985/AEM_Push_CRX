119b0425d42811efb0120d29e433f793b3c1ecce
G85 <email@example.com>
2024-08-05 20:08:24 +0100

    Added detect changes on subdirectories

--- Form1.cs
+++ Form1.cs
@@ -9,27 +9,6 @@ namespace AEM_Push_CRX
 {
     public partial class Form1 : Form
     {
-        // Declaraciones de la API de Windows
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        // Delegado y constantes para el hook
-        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-        private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x0100;
-        private LowLevelKeyboardProc _proc;
-        private IntPtr _hookID = IntPtr.Zero;
-
         private Curl curl;
         private Dictionary<string, string> fileHashes;
 
@@ -41,43 +20,9 @@ namespace AEM_Push_CRX
             //InitHooking();
         }
 
-        private void InitHooking()
-        {   
-            _proc = HookCallback;
-            _hookID = SetHook(_proc);
-        }
-
-
-        private IntPtr SetHook(LowLevelKeyboardProc proc)
-        {
-            using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
-            {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
-            }
-        }
-
-        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
-            {
-                int vkCode = Marshal.ReadInt32(lParam);
-                Keys key = (Keys)vkCode;
-
-                // Verificar si se presionan Control + S
-                if (Control.ModifierKeys == Keys.Control && key == Keys.S)
-                {
-                    Debug.WriteLine("Pulsado Control + S");
-                }
-            }
-            // Pasar el evento al siguiente hook en la cadena
-            return CallNextHookEx(_hookID, nCode, wParam, lParam);
-        }
-
         protected override void OnFormClosed(FormClosedEventArgs e)
         {
             // Desenganchar el hook cuando el formulario se cierre
-            UnhookWindowsHookEx(_hookID);
             base.OnFormClosed(e);
         }
 
@@ -93,7 +38,7 @@ namespace AEM_Push_CRX
             FileSystemWatcher watcher = new FileSystemWatcher
             {
                 Path = path,
-                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName , IncludeSubdirectories = true
             };
 
             // Suscribirse a los eventos
